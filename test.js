9b7faa8b6f0c98fef575fac0a55237e2f75dad8e
ganezasan <istaka12@gmail.com>
2019-03-11 10:58:36 +0900

    check CI_PULL_REQUEST

---
+++ test.js
@@ -0,0 +1,7 @@
+const IS_PULL_REQUEST =
+  process.env.CI_PULL_REQUEST || process.env.CIRCLE_PULL_REQUEST;
+
+if (IS_PULL_REQUEST) {
+  shell.echo('Skipping deploy on a pull request');
+  shell.exit(0);
+}
